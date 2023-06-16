aa97cf65031ad30691d30511a29b2486a666bdc9
zhangfuhao <>
2023-06-16 16:54:23 +0800

    feat: add style variable

---
+++ src/style/index.ts
@@ -0,0 +1,90 @@
+export const BLUE_1 = '#94c1ff';
+export const BLUE_2 = '#588bee';
+export const BLUE_3 = '#005ab6';
+
+export const GREEN_1 = '#17c28f';
+export const GREEN_2 = '#46dfaa';
+export const GREEN_3 = '#008c5d';
+
+export const YELLOW_1 = '#ffd45f';
+export const YELLOW_2 = '#fdb844';
+export const YELLOW_3 = '#c08300';
+
+export const RED_1 = '#ff988b';
+export const RED_2 = '#ee6159';
+export const RED_3 = '#b0272b';
+
+export const LIGHT_BLUE_1 = '#5de3ff';
+export const LIGHT_BLUE_2 = '#37c7e6';
+export const LIGHT_BLUE_3 = '#0091af';
+
+export const PINK_1 = '#ff8ac9';
+export const PINK_2 = '#de5193';
+export const PINK_3 = '#a30861';
+
+export const LIME_1 = '#9df26a';
+export const LIME_2 = '#81d54f';
+export const LIME_3 = '#489e10';
+
+export const PURPLE_1 = '#e495ff';
+export const PURPLE_2 = '#aa60e2';
+export const PURPLE_3 = '#732caa';
+
+// 用色顺序
+// N < 8
+export const COLORS_SMALL = [
+  BLUE_2,
+  GREEN_2,
+  YELLOW_2,
+  RED_2,
+  LIGHT_BLUE_2,
+  PINK_2,
+  LIME_2,
+  PURPLE_2,
+];
+// 8 < N < 24
+export const COLORS_MIDDLE = [
+  BLUE_2,
+  BLUE_1,
+  GREEN_2,
+  GREEN_1,
+  YELLOW_2,
+  YELLOW_1,
+  RED_2,
+  RED_1,
+  LIGHT_BLUE_2,
+  LIGHT_BLUE_1,
+  PINK_2,
+  PINK_1,
+  LIME_2,
+  LIME_1,
+  PURPLE_2,
+  PURPLE_1,
+];
+// N > 24
+export const COLORS_LARGE = [
+  BLUE_2,
+  BLUE_1,
+  BLUE_3,
+  GREEN_2,
+  GREEN_1,
+  GREEN_3,
+  YELLOW_2,
+  YELLOW_1,
+  YELLOW_3,
+  RED_2,
+  RED_1,
+  RED_3,
+  LIGHT_BLUE_2,
+  LIGHT_BLUE_1,
+  LIGHT_BLUE_3,
+  PINK_2,
+  PINK_1,
+  PINK_3,
+  LIME_2,
+  LIME_1,
+  LIME_3,
+  PURPLE_2,
+  PURPLE_1,
+  PURPLE_3,
+];
