372fd3d57e414f3c9d75aec9ee81a16250e3d5b3
wangyuhu <wangyuhu@sensoro.com>
2023-06-26 15:44:42 +0800

    feat(utils): 添加合并config方法

---
+++ src/utils/index.ts
@@ -0,0 +1,18 @@
+import type { BaseConfig } from '@ant-design/plots';
+import { merge } from 'lodash';
+
+type Config = BaseConfig<any>;
+
+export const mergeConfig = (
+  config: Config,
+  data: Record<string, any>[] | undefined,
+) => {
+  // TODO 这里是默认配置,各个组件默认配置不同，定义不同的配置项
+  // TODO 这里先不用UI规范，默认空对象，接收传入配置
+  const defaultConfig = {};
+
+  // TODO any需要替换，类型需要兼容所有组件的类型分类
+  return merge({}, defaultConfig, config, {
+    data,
+  }) as any;
+};
