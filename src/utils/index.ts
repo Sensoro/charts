086344c6475539796dfbb4089e982419005abe33
wangyuhu <wangyuhu@sensoro.com>
2023-06-26 18:05:59 +0800

    feat(utils): 适配矩形图类型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,7 +5,13 @@ type Config = BaseConfig<any>;
 
 export const mergeConfig = (
   config: Config,
-  data: Record<string, any>[] | undefined,
+  data:
+    | Record<string, any>[]
+    | {
+        name: string;
+        children: Record<string, any>[];
+      }
+    | undefined,
 ) => {
   // TODO 这里是默认配置,各个组件默认配置不同，定义不同的配置项
   // TODO 这里先不用UI规范，默认空对象，接收传入配置
