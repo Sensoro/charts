b1de5e92a35304d4b96484e4ec98c86099c68782
zhangfuhao <>
2023-07-03 11:58:59 +0800

    fix: line basic demo error

--- src/Line/demos/basic.tsx
+++ src/Line/demos/basic.tsx
@@ -4,9 +4,6 @@ import React, { useEffect, useState } from 'react';
 import { Line } from '@sensoro-design/charts';
 import EditorDemo from '../../../docs/components/Editor';
 
-import { Button } from 'antd';
-import { Space } from 'antd/es';
-
 export default () => {
   const [config, setConfig] = useState<LineConfig>({
     // 注释
@@ -36,26 +33,23 @@ export default () => {
   }, []);
 
   return (
-    <Space direction="vertical" size={12} style={{ width: '100%' }}>
-      <Button>隐藏配置栏</Button>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          columnGap: 16,
-          height: 500,
-        }}
-      >
-        <div style={{ width: '50%' }}>
-          <EditorDemo
-            value={JSON.stringify(config, null, 2)}
-            onChange={(v) => setConfig(JSON.parse(v as string))}
-          />
-        </div>
-        <div style={{ width: '50%' }}>
-          <Line {...config} />
-        </div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        columnGap: 16,
+        height: 500,
+      }}
+    >
+      <div style={{ width: '50%' }}>
+        <EditorDemo
+          value={JSON.stringify(config, null, 2)}
+          onChange={(v) => setConfig(JSON.parse(v as string))}
+        />
       </div>
-    </Space>
+      <div style={{ width: '50%' }}>
+        <Line config={config} />
+      </div>
+    </div>
   );
 };
