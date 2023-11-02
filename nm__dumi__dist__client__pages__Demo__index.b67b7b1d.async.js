7cb82505e1412dd684d6f97aec8872ea76d74e48
tiger5wang <tiger5wang@users.noreply.github.com>
2023-11-02 07:03:22 +0000

    deploy: 4bdbe6f303cdac4dad08b095341e0be116d0f38f

--- nm__dumi__dist__client__pages__Demo__index.b67b7b1d.async.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_sensoro_design_charts=self.webpackChunk_sensoro_design_charts||[]).push([[9],{55171:function(i,n,e){e.r(n),e.d(n,{default:function(){return t}});var s=e(85155),o=e(50959),r=function(){var d=(0,s.UO)(),u=d.id,m=(0,s.WF)(),c=m.demos,f=c[u]||{},a=f.component;return a&&(0,o.createElement)(a)},t=r}}]);
