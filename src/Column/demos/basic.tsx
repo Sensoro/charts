e088a1610e6e4252fd815b0f0f8000195d42d60d
wangyuhu <wangyuhu@sensoro.com>
2023-12-06 10:23:46 +0800

    feat: 柱状图圆角

--- src/Column/demos/basic.tsx
+++ src/Column/demos/basic.tsx
@@ -51,6 +51,74 @@ export default () => {
         alias: '销售额',
       },
     },
+    interactions: [{ type: 'element-active' }], // 设置 hover 效果
+    columnStyle: {
+      radius: [1, 1, 0, 0], // 设置柱状图的圆角，
+      // stroke: 'blue', // 柱状图的边框色
+      // fill: 'lightblue', // 柱状图的填充色
+    },
+    // interactions: [{ type: 'element-active' }], // 设置 hover 效果
+    // geometryOptions: [
+    //   {
+    //     geometry: 'column',
+    //     color: 'lightblue', // 柱状图的填充色
+    //     style: {
+    //       stroke: 'blue', // 柱状图的边框色
+    //     },
+    //   },
+    // ],
+    // // 自定义渲染函数
+    // geometry: {
+    //   column: {
+    //     label: {
+    //       // 自定义标签渲染函数
+    //       formatter: (datum) => {
+    //         if (datum.value > 0) {
+    //           return datum.value;
+    //         }
+    //         return ''; // 返回空字符串表示不显示标签
+    //       },
+    //     },
+    //     // 自定义柱状图渲染函数
+    //     customInfo: (datum, color) => {
+    //       if (datum.data.value > 0) {
+    //         return {
+    //           color: 'blue', // 设置柱状图的填充色
+    //           active: {
+    //             // 设置 hover 时的样式
+    //             style: {
+    //               fill: 'rgba(10, 27, 57, 0.04)', // 设置 hover 下空白占位的颜色
+    //             },
+    //           },
+    //         };
+    //       }
+    //       return {};
+    //     },
+    //   },
+    // },
+    // state: {
+    // // 设置 active 激活状态的样式
+    // active: {
+    // columnBackground: {
+    //   style: {
+    //     fill: '#000',
+    //     fillOpacity: 0.25,
+    //   },
+    // },
+    //   },
+    // },
+    // interactions: [
+    //   {
+    //     type: 'element-highlight', // 设置 hover 效果
+    //     cfg: {
+    //       active: {
+    //         style: {
+    //           fill: 'red', // 设置 hover 时的背景色
+    //         },
+    //       },
+    //     },
+    //   },
+    // ],
   });
 
   return (
